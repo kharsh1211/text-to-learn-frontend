7721ca57f07eb57faf3b3663b733765c9790bdfa
Kumar Harsh <kumar.co19@nsut.ac.in>
2026-01-18 10:22:57 +0530

    apiurl changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://text-to-learn-api.onrender.com/api' 
+  // apiUrl: 'https://text-to-learn-api.onrender.com/api' 
+  apiUrl:'https://backend-latest-kg42.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8080/api'
-  apiUrl: 'https://text-to-learn-api.onrender.com/api'
+  // apiUrl: 'https://text-to-learn-api.onrender.com/api'
+  apiUrl:'https://backend-latest-kg42.onrender.com/api'
 };
